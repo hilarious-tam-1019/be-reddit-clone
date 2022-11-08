63a3fffb852552f15d7629d7db2b69dba54086b6
funnytam1019 <tam.nguyen.10119@gmail.com>
2022-11-08 16:00:51 +0700

    adding docker config

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 import { Express } from 'express';
+import { initTRPC } from '@trpc/server';
 
 export async function createServer(): Promise<Express> {
   const server = express();
   server.use(express.json());
-  server.get('/', (req, res) => {
-    res.send('');
-  });
   return server;
 }
